c6f11ac4d0ec4f9eee74821177dcdc67353188aa
Nico Pretorius <nico.pretorius@pbtgroup.co.za>
2019-02-02 23:36:24 +0200

    Updated description text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React again!!
+            I was changed on the future branch
           </a>
         </header>
       </div>
